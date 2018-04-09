da6edae9254db97cb1054cf3ec78e1853e607d56
LikeLikeAteMyShield <jonnycameron17@gmail.com>
2018-04-09 01:44:43 +0100

    add pokemon title view

--- src/main/clojure/com/llams/clojangles/pokemon.clj
+++ src/main/clojure/com/llams/clojangles/pokemon.clj
@@ -5,4 +5,17 @@
   (:require [clojure.string :refer :all]))
 
 (defn display-pokemon [id]
-  (h 3 id))
+  (let [response (client/get (str "http://pokeapi.co/api/v2/pokemon/" id))
+        json (get response :body)
+        pokemon (parse-string json true)
+        sprites (get pokemon :sprites)]
+    (html
+      (head
+        (stylesheet "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"))
+      (body
+        (nav (css "navbar" "navbar-dark" "bg-dark")
+             (div (css "container")
+                  (a (css "navbar-brand") "/application/home" "Pokedex")))
+        (div (css "jumbotron")
+             (div (css "container")
+                  (h 1 (str (img (attribute "style" "width: 18rem;") {:src (get sprites :front_default) :alt "sprite"}) (strong (capitalize (:name pokemon)))))))))))
